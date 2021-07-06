68eb102cbdcbd96288c6a621bcd1be375e64287c
Yasmeen Hitti <yasmeen.hitti@mail.mcgill.ca>
2021-07-06 15:21:02 -0400

    setup of package

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='Plant_eV',
+    version ='1.0',
+    install_requires =["numpy","datetime","Adafruit_DHT","Picamera","RPi.GPIO",
+			"time","board","busio","h5py","pandas","paho"])
