f0541d916c8fe3bc69ae177a4696585d351aa60c
Yasmeen Hitti <yasmeen.hitti@mail.mcgill.ca>
2021-07-06 15:14:44 -0400

    logging foreverloop

--- mqtt_test.py
+++ mqtt_test.py
@@ -6,13 +6,16 @@ import paho.mqtt.client as mqtt
 # Setup callback functions that are called when MQTT events happen like 
 # connecting to the server or receiving data from a subscribed feed. 
 def on_connect(client, userdata, flags, rc): 
+   
    print("Connected with result code " + str(rc)) 
    # Subscribing in on_connect() means that if we lose the connection and 
    # reconnect then subscriptions will be renewed. 
-   client.subscribe("test") 
+   client.subscribe("test")
+   ev_data = -1    
 # The callback for when a PUBLISH message is received from the server. 
 def on_message(client, userdata, msg): 
-   print(msg.topic+" "+str(msg.payload)) 
+     
+    print(msg.topic+" "+str(msg.payload)) 
    # Check if this is a message for the Pi LED. 
          
 # Create MQTT client and connect to localhost, i.e. the Raspberry Pi running 
@@ -22,4 +25,4 @@ client.on_connect = on_connect
 client.on_message = on_message 
 client.connect('192.168.0.106', 1883, 60) 
 # Connect to the MQTT server and process messages in a background thread. 
-client.loop_start() 
+client.loop_forever() 
