53b4d0bccc374e33867f90628fddc1b7fe08026f
Yasmeen Hitti <yasmeen.hitti@mail.mcgill.ca>
2021-07-08 13:42:34 -0400

    fixed file for cam logging

--- Plant_eV/env_conditions.py
+++ Plant_eV/env_conditions.py
@@ -18,7 +18,7 @@ import pandas as pd
 import RPi.GPIO as GPIO
 
 
-PATH = 'home/pi/Pictures/Plant_eV/'
+PATH = './Pictures/rpi/'
 
 def check_day(t1,t2):
 	print("what type is this",type(t1.day()))
@@ -27,25 +27,30 @@ def check_day(t1,t2):
 		return day_root
 	else:
 		return t2.strftime("%Y%m%d")
-
+def check_path(path):
+        if os.path.exists(path):
+                return path
+        else:
+                os.makedirs(path)
+                return path
 class Sensors(object):
-	def __init__(self, path = PATH):
-		self.path = path
-		self.timestamp = datetime.now()
+        def __init__(self, path = PATH):
+                self.path = path
+                self.timestamp = datetime.fromtimestamp(time.time())
 
-	def hdf5_exist(self,root):
-		# Check if file exists and have it in write option
+        def hdf5_exist(self,root):
+                # Check if file exists and have it in write option
 
-		if os.path.exists(root+'rpi1_env_data.h5'):
-			f = pd.read_hdf(root+'rpi1_env_data.h5',mode = 'a')
-		else:
-		# Create file if this it doesn't exist
+                if os.path.exists(root+'rpi1_env_data.h5'):
+                        f = pd.read_hdf(root+'rpi1_env_data.h5',mode = 'a')
+                else:
+                # Create file if this it doesn't exist
 
-			df = pd.DataFrame(columns=['time','co2','voc','rh','temp'])
-			f = pd.HDFStore(root+'rpi1_env_data.h5')
-			f.append('rp1',df)
+                        df = pd.DataFrame(columns=['time','co2','voc','rh','temp'])
+                        f = pd.HDFStore(root+'rpi1_env_data.h5')
+                        f.append('rp1',df)
 
-		return f
+                return f
 
 	def create_Folder(self, directory):
 		try:
@@ -58,17 +63,20 @@ class Sensors(object):
 
 
 	def picture(self):
+                t = int(time.time())            #get time
+                t = datetime.fromtimestamp(t)
+                #print('what type is t1',t.day)
+                #print("t1,t2",t,self.timestamp)
+                day_root = check_day(t,self.timestamp) #check if we are in the same day
+                print("what is day root",day_root)
+                self.create_Folder(self.path+day_root)
+                self.timestamp = t                     # update previous timestep to now 
+                camera = PiCamera()
+                day = t.strftime("%Y%m%d_%H_%M_%S")
+                camera.start_preview()
+                camera.capture(self.path+day_root+'/'+str(day)+'.jpg')
+                sleep(.2)		
 		t = datetime.now()
-		#tday = t.day()
-		print('what type is t1',t.day)
-		print("t1,t2",t,self.timestamp)
-		day_root = check_day(t,self.timestamp)
-		self.timestamp = t
-		camera = PiCamera()
-		day = int(time.time())
-		camera.start_preview()
-		camera.capture(self.root+day_root+'/'+str(int(day))+'.jpg')
-		sleep(.2)
 		camera.stop_preview()
 		
 	def gas(self):
@@ -110,27 +118,35 @@ class Sensors(object):
 
 def main():
 # Sensor activating
-	S = Sensors()
-	print("what is this",datetime.now().day)
-	#root = "/home/pi/sensors/"	 # define root
-	time_id = time.time()
-	#root = '/home/pi/sensors/' # define root
-	#data = pd.DataFrame({'time':time_id,'co2':S.gas()[0],'voc':S.gas()[1],'rh':S.rh_temp()[0],'temp':S.rh_temp()[1]})
-	## add to hdf5
-	S.picture()
-	print('temp',S.rh_temp()[1],'humidity',S.rh_temp()[0])
-	co2 = S.gas()[0]
-	voc = S.gas()[1]
-	light = S.light_intensity()
-	#time_id = time.time()
-	#root = '/home/pi/sensors/' # define root
-	#data = pd.DataFrame({'time':time_id,'co2':S.gas()[0],'voc':S.gas()[1],'rh':S.rh_temp()[0],'temp':S.rh_temp()[1]})
-	## add to hdf5
-	print(co2,"what is co2")
-	print(voc,"what is voc")
-	print(time_id)
-	print(light, "what is light")
+        S = Sensors()
+        #print(time.time())
+        print("what is this",datetime.now().day)
+        t = int(time.time())
+        t = datetime.fromtimestamp(t)
+        t =t.strftime("%Y%m%d_%H_%M_%S")        
+        print(t)
+
+        #root = "/home/pi/sensors/"      # define root
+        time_id = time.time()
+        #root = '/home/pi/sensors/' # define root
+        #data = pd.DataFrame({'time':time_id,'co2':S.gas()[0],'voc':S.gas()[1],'rh':S.rh_temp()[0],'$
+        ## add to hdf5
+        S.picture()
+        print('temp',S.rh_temp()[1],'humidity',S.rh_temp()[0])
+        co2 = S.gas()[0]
+        voc = S.gas()[1]
+        light = S.light_intensity()
+        #time_id = time.time()
+        #root = '/home/pi/sensors/' # define root
+        #data = pd.DataFrame({'time':time_id,'co2':S.gas()[0],'voc':S.gas()[1],'rh':S.rh_temp()[0],'$
+        ## add to hdf5
+        print(co2,"what is co2")
+        print(voc,"what is voc")
+        print(time_id)
+        print(light, "what is light")
 
 
-if __name__ == '__main__':
-	main()
+#if __name__ == '__main__':
+#       main()
+
+
