77f150576b5b244c898243a1830e7c0261ba8d79
Yasmeen Hitti <yasmeen.hitti@mail.mcgill.ca>
2021-07-08 16:18:22 -0400

    fixed typo

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ setup(
     name='Plant_eV',
     version ='1.0',
     install_requires =["numpy","datetime","Adafruit_DHT","Picamera","RPi.GPIO",
-			,"board","h5py","pandas"])
+			"board","h5py","pandas"])
