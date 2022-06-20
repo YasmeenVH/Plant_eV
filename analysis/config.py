ca652b014b607615759e2bfe223821f069e49c0f
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2022-06-20 17:32:05 -0400

    config file for dataloader and running analysis

---
+++ analysis/config.py
@@ -0,0 +1,22 @@
+
+file_type = ["csv",'hdf5'] # file type can be csv or hdf5 format
+path_official_results = '/home/y/Documents/officialresults'
+official_blue = ['blue1','blue2','blue3']
+official_red = ['red1','red2','red3']
+official_amber = ['amber1','amber2','amber3']
+
+dcbias_results = '/home/y'
+dcbias_blue = 'blue150umol'
+dcbias_amber = 'amber150umol'
+dcbias_red = 'red150umol'
+
+blank_blue = 'blue'
+blank_red = 'red'
+blank_amber = 'amber'
+
+res1 = '100Kohm'
+res2 = '1Mohm'
+res3 = '10Mohm'
+
+n_points = 3000
+
