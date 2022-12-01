4d64cb53ce8b1d4f16f6f63df4cb5ff9b35f9713
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2022-12-01 16:09:00 -0500

    config file:

--- analysis/config.py
+++ analysis/config.py
@@ -1,14 +1,24 @@
 
 file_type = ["csv",'hdf5'] # file type can be csv or hdf5 format
 path_official_results = '/home/y/Documents/officialresults'
-official_blue = ['blue1','blue2','blue3']
+official_blue = ['blue1','p2blue','p3blue']
 official_red = ['red1','red2','red3']
 official_amber = ['amber1','amber2','amber3']
+official_blanks = []
+
+
+wg = ['wg1blue','wg2blue','wg3blue']
+al = ['al1blue','al2blue']#'al3blue']
+newblue = ['p1blue','p2blue','p3blue']
+
+inverse = 'inverse'
+notinverse = 'notinverse'
 
 dcbias_results = '/home/y'
 dcbias_blue = 'blue150umol'
 dcbias_amber = 'amber150umol'
 dcbias_red = 'red150umol'
+dcblanks = 'blanks'
 
 blank_blue = 'blue'
 blank_red = 'red'
